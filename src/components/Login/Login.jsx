38ff8036e2a8b6f2ab84c03bc05f1b15601d9e57
HeadShotDevil <88618221+HeadShotDevil@users.noreply.github.com>
2023-01-29 22:17:03 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import './Login.css'
 import {Link,Outlet} from 'react-router-dom'
-import {Row,Col} from 'react-bootstrap'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Login() {
   return (
-    <div>
-      <Row xs={1} md={2}>
-        <Col>Home</Col>
-        <Col>Peeku</Col>
+    <div className="login">
+      <Row xs={1} md={2} className="ro">
+        <Col className='columns'>
+          <h1 className='heads'>For <span className='text-success'>Clubs</span></h1>
+          <h4 className='mb-5'>login to create new events...</h4>
+          <button className='btn1'>
+            <Link className='nav-link' to='/studentlogin'>Login</Link>
+          </button>
+          <h4>Don't have an account?</h4>
+          <h3><Link className="nav-link" to='/register'>Sign Up</Link></h3>
+        </Col>
+        <Col className='columns'>
+          <h1 className='heads'>For <span className='text-primary'>Students</span></h1>
+          <h4 className='mb-5'>login for news updates...</h4>
+          <button className='btn2'>
+          <Link className="nav-link" to='/clublogin'>Login</Link>
+          </button>
+          <h4>Don't have an account?</h4>
+          <h3><Link className="nav-link text-success" to='/register'>Sign Up</Link></h3>
+        </Col>
       </Row>
       {/* <ul className="nav justify-content-center">
         <li className="nav-item">
